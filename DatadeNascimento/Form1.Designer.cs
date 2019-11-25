bc6312a6eaa85db9cef269c80e120b70212f17fc
BD1 <brunodesa288@gmail.com>
2019-11-25 17:21:23 +0000

    Adicionar arquivos de projeto.

---
+++ DatadeNascimento/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DatadeNascimento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(295, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 61);
+            this.label2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(298, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 63);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Data de Nascimento";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 69);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DatadeNascimento/Idade1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatadeNascimento
+{
+    class Idade1
+    {
+        private DateTime data = new DateTime().Date;
+
+
+        public string recebe
+        {
+            set
+            {
+                data = Convert.ToDateTime(value);
+            }
+        }
+
+        public string envia
+        {
+            get
+            {
+                DateTime today = DateTime.Now;
+
+                int anos = today.Year - data.Year;
+                int meses = today.Month - data.Month;
+                int dias = today.Day - data.Day;
+
+                if (dias < 0)
+                {
+                    meses--;
+                    dias += 30;
+                }
+
+                if (meses < 0)
+                {
+                    anos--;
+                    meses += 12;
+                }
+
+                string frase = "A sua idade é ";
+                frase += anos.ToString() + " anos ";
+                frase += meses.ToString() + " meses e ";
+                frase += dias.ToString() + " dias. ";
+
+
+                return frase + "O dia da semana em que nasceu foi " + data.ToString("dddddd");
+            }
+        }
+
+        private string diadasemana
+        {
+            get
+            {
+                string frase = "O dia da semana em que nasceu é ";
+                frase += data.ToString("dddd") + ".\n";
+                return frase;
+            }
+        }
+
+
+        private string idade
+        {
+            get
+            {
+                DateTime hoje = DateTime.Now;
+                TimeSpan dif = hoje - data;
+                DateTime idade = DateTime.MinValue + dif;
+                //MinValue = 1/1/1
+                string frase = "A sua idade é";
+                frase += (idade.Year - 1) + " anos, ";
+                frase += (idade.Month - 1) + "meses é";
+                frase += (idade.Day - 1) + "dias,";
+
+                return frase;
+            }
+        }
+
+        private string sign
+        {
+            get
+            {
+                string s = "";
+                int d = data.Month * 100 + data.Day;
+                if (d < 121 | d > 1221) s = "Sagitario";
+                else if (d < 219) s = "Aquario";
+                else if (d < 219) s = "Peixe";
+                else if (d < 321) s = "Carneiro";
+                else if (d < 420) s = "Touro";
+                else if (d < 521) s = "Gémeos";
+                else if (d < 621) s = "Caranguejo";
+                else if (d < 723) s = "Leâo";
+                else if (d < 823) s = "Virgem";
+                else if (d < 923) s = "Aquario";
+                else if (d < 1022) s = "Balança";
+                else if (d < 1122) s = "Escorpião";
+                else if (d < 1222) s = "Sagitário";
+                string frase = "O seu signo é ";
+                frase += s + ".\n";
+                return frase;
+            }
+        }
+
+        public string envia1
+        {
+            get
+            {
+                return diadasemana + idade + sign;
+            }
+        }
+    }
+}
